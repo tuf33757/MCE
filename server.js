7c8afbb2c8b3dafe19f6e212a9926066eadcee9f
Robert J Zahorchak <tuf33757@temple.edu>
2016-09-07 17:17:48 -0400

    adding routing and passport support

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-var express = require('express');
+/*var express = require('express');
 var app = express();
 
 app.use(express.static(__dirname + '/public'));
@@ -8,4 +8,42 @@ app.use('/', express.static(__dirname + '/public/index.html'));
 var port = process.env.PORT || 3030;
 app.listen(port, function() {
     console.log('Listening on port ' + port);
-});
+});*/
+
+var express  = require('express');
+var app      = express();
+var port     = process.env.PORT || 8080;
+
+var passport = require('passport');
+var flash    = require('connect-flash');
+
+var morgan       = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser   = require('body-parser');
+var session      = require('express-session');
+
+var configDB = require('./config/database.js');
+
+
+
+// require('./config/passport')(passport); // pass passport for configuration
+
+// set up our express application
+app.use(morgan('dev')); // log every request to the console
+app.use(cookieParser()); // read cookies (needed for auth)
+app.use(bodyParser()); // get information from html forms
+
+//app.set('view engine', 'ejs'); // set up ejs for templating
+
+// required for passport
+app.use(session({ secret: 'ilovescotchscotchyscotchscotch' })); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
+
+// routes ======================================================================
+require('./public/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
+
+// launch ======================================================================
+app.listen(port);
+console.log('The magic happens on port ' + port);
